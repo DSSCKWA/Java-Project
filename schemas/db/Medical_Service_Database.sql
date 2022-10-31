2f4e5d84b6eb36e84488deae87826b4ee300ed76
Wojciech_Malicki <wojciech.malicki09@gmail.com>
2022-10-31 18:30:02 +0100

    Added Database

---
+++ schemas/db/Medical_Service_Database.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 31 Paź 2022, 18:22
+-- Wersja serwera: 10.4.25-MariaDB
+-- Wersja PHP: 8.0.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `medical_clinic`
+--
+CREATE DATABASE IF NOT EXISTS `medical_service` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `medical_service`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `clinics`
+--
+
+CREATE TABLE `clinics` (
+  `clinic_id` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `doctor_id` int(11) NOT NULL,
+  `clinic_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `expertise`
+--
+
+CREATE TABLE `expertise` (
+  `doctor_id` int(11) NOT NULL,
+  `area_of_expertise` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `phone_number` int(9) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `permissions` enum('admin','moderator','patient','doctor','guest') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `visits`
+--
+
+CREATE TABLE `visits` (
+  `status` enum('completed','pending','canceled') NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `client_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `clinics`
+--
+ALTER TABLE `clinics`
+  ADD PRIMARY KEY (`clinic_id`);
+
+--
+-- Indeksy dla tabeli `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`doctor_id`,`clinic_id`),
+  ADD KEY `clinic_ID` (`clinic_id`);
+
+--
+-- Indeksy dla tabeli `expertise`
+--
+ALTER TABLE `expertise`
+  ADD PRIMARY KEY (`doctor_id`,`area_of_expertise`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indeksy dla tabeli `visits`
+--
+ALTER TABLE `visits`
+  ADD PRIMARY KEY (`date`,`time`,`client_id`,`doctor_id`),
+  ADD KEY `client_ID` (`client_id`),
+  ADD KEY `doctor_ID` (`doctor_id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `clinics`
+--
+ALTER TABLE `clinics`
+  MODIFY `clinic_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `doctors`
+--
+ALTER TABLE `doctors`
+  ADD CONSTRAINT `doctors_ibfk_1` FOREIGN KEY (`clinic_ID`) REFERENCES `clinics` (`clinic_ID`),
+  ADD CONSTRAINT `doctors_ibfk_3` FOREIGN KEY (`doctor_ID`) REFERENCES `users` (`user_ID`);
+
+--
+-- Ograniczenia dla tabeli `expertise`
+--
+ALTER TABLE `expertise`
+  ADD CONSTRAINT `expertise_ibfk_1` FOREIGN KEY (`doctor_ID`) REFERENCES `doctors` (`doctor_ID`),
+  ADD CONSTRAINT `expertise_ibfk_2` FOREIGN KEY (`doctor_ID`) REFERENCES `doctors` (`doctor_ID`);
+
+--
+-- Ograniczenia dla tabeli `visits`
+--
+ALTER TABLE `visits`
+  ADD CONSTRAINT `visits_ibfk_1` FOREIGN KEY (`client_ID`) REFERENCES `users` (`user_ID`),
+  ADD CONSTRAINT `visits_ibfk_2` FOREIGN KEY (`doctor_ID`) REFERENCES `doctors` (`doctor_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
