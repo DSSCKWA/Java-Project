7afbe5d03e1a54f952d1f8dfb38388b510b30a8a
Wojciech_Malicki <wojciech.malicki09@gmail.com>
2022-11-27 16:38:47 +0100

    commit z vscode

--- src/schedule/Schedule.java
+++ src/schedule/Schedule.java
@@ -1,4 +1,32 @@
 package src.schedule;
 
+import src.clinic.Clinic;
+import src.db.client.DBClient;
+import src.db.repository.DoctorsRepository;
+import src.db.tables.DoctorsTable;
+
+import java.sql.SQLException;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
 public class Schedule {
+    private int doctorId;
+    private int clinicId;
+    private DayOfWeek day; // jednostki czasu
+    private LocalTime startTime;// jednostki czasu
+    private LocalTime endTime;// jednostki czasu
+
+
+    public Schedule(int doctorId,int clinicId,DayOfWeek day, LocalTime startTime, LocalTime endTime)
+    {
+
+    }
+
+
+
+
+
+
+
+
 }
--- src/users/Doctor.java
+++ src/users/Doctor.java
@@ -1,53 +1,132 @@
 package src.users;
 
 import src.clinic.Clinic;
+import src.db.client.DBClient;
+import src.db.repository.DoctorsRepository;
+import src.db.repository.ScheduleRepository;
+import src.db.tables.DoctorsTable;
+import src.db.tables.ScheduleTable;
+import src.schedule.Schedule;
+import src.visit.Status;
+import src.visit.Visit;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
+import java.sql.SQLException;
+import java.time.DayOfWeek;
+import java.util.ArrayList;
 
-public class Doctor extends User{
+public class Doctor extends User {
+    private ArrayList<Clinic> doctorClinics;
+    private ArrayList<Schedule> doctorSchedules;
+    private ArrayList<Visit> doctorVisits;
+    private final DBClient dbClientAutoCommit;
 
+    public Doctor(String name, String surname, String address, String city, int phoneNumber, String email, String password) {
+        super(name, surname, address, city, phoneNumber, email, password, Permissions.DOCTOR);
+        this.doctorClinics = new ArrayList<Clinic>();
+        this.doctorSchedules = new ArrayList<Schedule>();
+        this.doctorVisits = new ArrayList<Visit>();
+        try {
+            dbClientAutoCommit = new DBClient(true);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void commitChanges() {
 
-    private List<Integer> expertiseId;
-    private HashSet<Integer> clinicId;
-    public List<Integer> getExpertiseId() {
-        return expertiseId;
+        for (Clinic x:doctorClinics
+             ) {
+            DoctorsTable doctor = new DoctorsTable(userId, x.getClinicId());
+            try
+
+            {
+                DBClient dbClientAutoCommit = new DBClient(true);
+                DoctorsRepository doctorsRepository = new DoctorsRepository(dbClientAutoCommit);
+                doctorId = doctorsRepository.insertDoctor(doctor); // nie wiem o co chodzi
+                doctor.setDoctorId(userId);
+                System.out.println(doctorsRepository.getAllDoctors());
+            } catch(SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
+        for (Schedule x:doctorSchedules
+        ) {
+            ScheduleTable schedule = new ScheduleTable(userId, x.getClinicId(),x.getDay(),x.getStartTime(),x.getEndTime());
+            try
+            {
+                DBClient dbClientAutoCommit = new DBClient(true);
+                ScheduleRepository scheduleRepository = new ScheduleRepository(dbClientAutoCommit);
+                scheduleRepository.insertSchedule(schedule); // nie wiem o co chodzi
+                System.out.println(scheduleRepository.getAllSchedules());
+
+            } catch(SQLException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+public void addClinic(Clinic clinic)
+{
+    doctorClinics.add(clinic);
+}
+    public void removeClinic(Clinic clinic)
+    {
+        boolean exists= false;
+        for (Clinic x:doctorClinics) {
+            if(x.equals(clinic))
+            {
+                exists=true;
+                break;
+            }
+        }
+        if(exists)
+        {
+            ///TODO
+        }
     }
 
-    public void setExpertiseId(List<Integer> expertiseId) {
-        this.expertiseId = expertiseId;
+
+
+public void cancelVisit(Visit visit)
+{
+    visit.setStatus(Status.CANCELED);
+}
+public void postponeVisit(Visit visit,Date date,Time startTime,Time endTime)
+    {
+        visit.setDate(date);
+        visit.setStartTime(startTime);
+        visit.setEndTime(startTime);
+    }
+    public void completeVisit(Visit visit)
+    {
+        visit.setStatus(Status.COMPLETED);
     }
 
-    public HashSet<Integer> getClinicId() {
-        return clinicId;
+    public void addToSchedule( int clinicId, DayOfWeek day, Time startTime, Time endTime)
+    {
+        boolean exists=false;
+        for (Schedule x:doctorSchedules) {
+            if(day==x.getDay() && ((startTime<x.getEndTime() && startTime>x.getStartTime())||(endTime<x.getEndTime() && endTime>x.getStartTime())))
+            {
+                exists=true;
+                break;
+            }
+        }
+        if(!exists)
+        {
+            doctorSchedules.add(new Schedule(userID,clinicId,day,startTime,endTime));
+        }
     }
 
-    public void setClinicId(HashSet<Integer> clinicId) {
-        this.clinicId = clinicId;
-    }
+    public void removeFromSchedule( Schedule schedule)
+    {
+        ScheduleRepository x= new ScheduleRepository(dbClientAutoCommit);
+        x.deleteSchedule(doctorId,clinicId,day) ///TODO bo jest niedokończone
+    };
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Doctor doctor = (Doctor) o;
-        User user = (User) o;
-        return expertiseId == doctor.expertiseId && clinicId == doctor.clinicId && super.equals(user);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), expertiseId, clinicId);
-    }
-
-    @Override
-    public String toString() {
-        return "Doctor{" +
-                super.toString() +
-                ", expertiseId='" + expertiseId + '\'' +
-                ", clinicId='" + clinicId + '\'' +
-                '}';
+    public void addVisit(Visit visit)
+    {
+        doctorVisits.add(visit);
     }
 }
