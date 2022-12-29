3bbe643f5e178cc42b7aa321fd376675ea648f4e
Karolina <karolinajez21@gmail.com>
2022-12-29 21:48:26 +0100

    Add GET, POST, PUT, DEL clinics requests

--- src/http/TestHttpClient.java
+++ src/http/TestHttpClient.java
@@ -1,6 +1,6 @@
 package src.http;
 
-import com.google.gson.Gson;
+import src.clinic.Clinic;
 
 import java.io.IOException;
 
@@ -11,5 +11,17 @@ public class TestHttpClient {
         System.out.println(httpClient.getClinics());
         System.out.println(httpClient.getUsers());
         System.out.println(httpClient.getEquipment());
+
+        Clinic clinic = new Clinic(1, "Super Hiper Fajna Klinika", "ul. Testowa 12", "Bydgoszcz");
+        httpClient.updateClinic(clinic);
+        System.out.println(httpClient.getClinics());
+        System.out.println(httpClient.getClinic(1));
+
+        Clinic clinic2 = new Clinic("Fajna Klinika", "ul. Testowa 1", "Wawa");
+        System.out.println(httpClient.addClinic(clinic2));
+        System.out.println(httpClient.getClinics());
+
+        System.out.println(httpClient.deleteClinic(5));
+        System.out.println(httpClient.getClinics());
     }
 }
